bf2e09674b0b44882b7cf7770de951a9260e4420
Ayush <ayush.taiga@gmail.com>
2024-10-13 12:56:44 +0530

    First version of our Hotels

---
+++ routes/menuRoutes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+const MenuItem = require("./../models/MenuItem");
+
+router.post("/", async (req, res) => {
+  try {
+    const data = req.body; //asuming that the body contains the person data
+
+    //create a new Person document using the Mongoose model
+    const newItem = new MenuItem(data);
+
+    //save the new Person in the database
+    const response = await newItem.save();
+    console.log("Data Saved");
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//GET method to get the Menu data
+router.get("/", async (req, res) => {
+  try {
+    const data = await MenuItem.find();
+    console.log("Data fetched");
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/:tasteType", async (req, res) => {
+  try {
+    const tasteType = req.params.tasteType; //Extract the worktype from the URL parameter
+    if (tasteType == "sweet" || tasteType == "spicy" || tasteType == "sour") {
+      const response = await MenuItem.find({ taste: tasteType });
+      console.log("response fetched");
+      res.status(200).json(response);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Invalid work Type" });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const db = require("./db");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello! Welcome to our Hotel, how can we help you ?");
+});
+
+app.get("/chicken", function (req, res) {
+  res.send("your chicken is ready");
+});
+
+//Import the router files
+const personRoutes = require("./routes/personRoutes");
+const menuRoutes = require("./routes/menuRoutes");
+
+//Use the routers
+app.use("/person", personRoutes);
+app.use("/menu", menuRoutes);
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
